76cad2b6df381dc3a7253a278e2f44bb872aec15
Valentin Schnabl <e11848108@student.tuwien.ac.at>
2023-11-20 10:56:06 +0100

    [1.1][F] add apollo angular to project scope
    Also adjusted cors policy in backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,54 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Apollo} from "apollo-angular";
+import {gql} from "@apollo/client";
 
+
+
+interface User {
+  id: number;
+  email: string
+  name: string
+  password: string
+  userType: UserType
+}
+
+enum UserType {
+  "Administrator",
+"ListingConsumer",
+"ListingProvider"
+}
+
+
+interface GetAllUsers {
+  users: User[];
+}
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'thesito';
+  users:User[] = []
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo.watchQuery<GetAllUsers>({
+      query: 	gql`{
+        getAllUsers{
+          name
+          id
+          email
+        }
+      }`
+    })
+      .valueChanges
+      .subscribe(({ data, loading }) => {
+        // @ts-ignore
+        this.users = data.getAllUsers;
+        console.log(data)
+        console.log(this.users)
+      });
+  }
+
 }
