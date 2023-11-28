fba71c99ad9eca20e8593ced8ffc79e1f8172ceb
Lorenz Kofler <e51850080@students.tuwien.ac.at>
2023-11-28 20:33:24 +0100

    [10.2] Add testdata (3002 theses)

---
+++ test_data/generate_testdata.py
@@ -0,0 +1,120 @@
+from openai import OpenAI
+import random
+import datetime
+import csv
+client = OpenAI()
+
+qualification = ['None', 'Bachelors', 'Masters', 'PhD']
+universities = ["Sofa State University (SSU),", "University of Underwater Basket Weaving (UUBW)",
+                "Couch Potato College (CPC)", "Pajama Party Institute (PPI)",
+                "Procrastination University (PU)", "Extra Terrestrial Academy (ETA)",
+                "Academy of Advanced Armchair Philosophy (AAAP)", "Bigfoot School of Natural Mysteries (BSNM)"
+                , "Institute of Imaginary Inventions (III)", "School of Selective Studies (SSS)", "Hibernation University (HU)",
+                "College of Comical Arts (CCA)", "University of Unlimited Naps (UUN)", "Virtual Reality Varsity (VRV)",
+                "Academy for Aspiring Superheroes (AAS)"]
+
+title_message = "Provide me with a research title or thesis topic. Add a touch of humor to it, please. It should relate to: "
+description_message = """Generate a compelling and humorous research topic
+description that spans around 200 words. This description should outline the
+potential benefits and advancements that could arise from the proposed research
+while also encouraging student engagement.  The description should align with a
+specific title:"""
+name_message = "Suggest a plausible name for a university professor, chosen at random."
+times = 50
+count = 20
+
+def get_random_tag():
+    with open("tags_en.csv", mode='r') as tagscsv:
+        csv_reader = csv.reader(tagscsv, delimiter=";")
+        all_rows = list(csv_reader)
+        random_row = random.choice(all_rows)
+        return random_row[3],random_row[1]
+
+def generate_titles(tags):
+    titles = []
+    title_prompts = []
+    for tag in tags:
+        title_prompts.append(title_message + tag)
+    r = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=title_prompts,
+        max_tokens=30,
+        temperature=0.9
+    )
+    for text in r.choices:
+        title = text.text.strip().replace('"','')
+        titles.append(title)
+    return titles
+
+
+def generate_descriptions(titles):
+    descriptions = []
+    description_prompts = []
+    for title in titles:
+        description_prompts.append(description_message + title)
+    r = client.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=description_prompts,
+        max_tokens=400,
+        temperature=0.9
+    )
+    for text in r.choices:
+        descriptions.append(text.text)
+    return descriptions
+
+def generate_advisors():
+    advisors = []
+    name_prompts = [name_message] * count
+    r = client.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt=name_prompts,
+    max_tokens=200,
+    temperature=0.9
+    )
+    for text in r.choices:
+        advisors.append(text.text)
+    return advisors
+
+def generate_random_date():
+    year = random.randint(2010, 2023)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)  
+    return datetime.date(year, month, day).isoformat()
+
+
+def run():
+    tag_names = []
+    tag_ids = []
+    for _ in range(count):
+        random_tag = get_random_tag()
+        tag_names.append(random_tag[0])
+        tag_ids.append(random_tag[1])
+
+    titles = generate_titles(tag_names)
+    descriptions = generate_descriptions(titles)
+    advisors = generate_advisors()
+
+    header = ['Title', 'Details', 'Qualification_Type', 'Date', 'University', 'Advisor', 'tag_id', 'tag_name']
+
+    with open('generated_test_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(header)
+        for _ in range(times):
+            for i in range(count):
+                university = random.choice(universities)
+                qualification_type = random.choice(qualification)
+                date = generate_random_date()
+                tag_id = tag_ids[i]
+                tag_name = tag_names[i]
+                title = titles[i]
+                details = descriptions[i]
+                advisor = advisors[i]
+                row = [title, details, qualification_type, date, university, advisor, tag_id, tag_name]
+                writer.writerow(row)
+                csvfile.flush()
+        print("done")
+
+run()
+
+
+
---
+++ test_data/insert_into_db.py
@@ -0,0 +1,49 @@
+import csv
+import psycopg2
+
+
+params = {
+    'dbname': 'ase_db',
+    'user': 'postgres',
+    'password': 'ase',
+    'host': '172.18.0.2'
+}
+
+def insert_data(row):
+    # Extracting data from the row
+    title, details, qualification_type, date, university, advisor, tag_id, tag_name = row
+
+    # Connect to the database
+    conn = psycopg2.connect(**params)
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+            INSERT INTO listings (title, details, requirement, created_at)
+            VALUES (%s, %s, %s, %s) RETURNING id;
+        """, (title, details, qualification_type, date))
+        listing_id = cursor.fetchone()[0]
+
+        cursor.execute("""
+            INSERT INTO listing_tags (listing_id, tag_id)
+            VALUES (%s, %s);
+        """, (listing_id, tag_id))
+
+        # Commit the transaction
+        conn.commit()
+    except Exception as e:
+        print("An error occurred:", e)
+        conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
+
+# Read the CSV file and insert data into the database
+with open('testdata.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Skip the header row
+    for row in reader:
+        insert_data(row)
+
+print("Data insertion complete.")
+
