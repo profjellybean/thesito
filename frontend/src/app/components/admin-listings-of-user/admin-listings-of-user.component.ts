985005824c53a04bc56c07cf1eccdef846641f99
Edon Govori <edon.govori@tuwien.ac.at>
2024-01-29 03:09:39 +0100

    [3.3][F] add delete listing for administrator

--- frontend/src/app/components/admin-listings-of-user/admin-listings-of-user.component.ts
+++ frontend/src/app/components/admin-listings-of-user/admin-listings-of-user.component.ts
@@ -2,13 +2,22 @@ import {Component, Inject, InjectionToken, OnInit, ViewChild} from '@angular/cor
 import {ListingService} from "../../services/listing.service";
 import {Router} from "@angular/router";
 import {AuthService} from "../../services/auth.service";
-import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from "@angular/material/dialog";
 import {Listing} from "../../models/Listing";
 import {MatPaginator} from "@angular/material/paginator";
 import {MatTableDataSource} from "@angular/material/table";
 import {LanguageService} from "../../services/language.service";
 import {EditListingComponent} from "../edit-listing/edit-listing.component";
 import {DetailComponent} from "../listing-details/detail.component";
+import {
+  DeleteConfirmationDialogComponent
+} from "../delete-confirmation-dialog-listing/delete-confirmation-dialog.component";
+import {User} from "../../models/User";
+import {
+  DeleteConfirmationDialogUserComponent
+} from "../delete-confirmation-dialog-user/delete-confirmation-dialog-user.component";
+import {TranslateService} from "@ngx-translate/core";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 
 @Component({
@@ -35,6 +44,8 @@ export class AdminListingsOfUserComponent implements OnInit{
   constructor(
     private listingService: ListingService,
     private router: Router,
+    private translateService: TranslateService,
+    private snackBar: MatSnackBar,
     private authService: AuthService,
     @Inject(MAT_DIALOG_DATA) public data: { userId: number },
     protected languageService: LanguageService,
@@ -85,8 +96,45 @@ export class AdminListingsOfUserComponent implements OnInit{
       data: { listingId: listingId},
     });}
 
-  deleteListing(listingId: number){
-    return;
+
+  deleteListing(listing: Listing): void {
+    let dialogRef: MatDialogRef<DeleteConfirmationDialogComponent>;
+
+    dialogRef = this.dialog.open(DeleteConfirmationDialogComponent, {
+      data: { title: listing?.title }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.listingService.deleteListingById(Number(listing?.id)).subscribe({
+          next: () => {
+            this.showSuccessMessage();
+            this.dataSource.data = this.dataSource.data.filter(u => u.id !== listing.id);
+            // Reload the page after successful deletion
+            //set 2s timeout to wait for the snackbar to disappear
+            setTimeout(() => location.reload(), 2000);
+          },
+          error: (error) => {
+            this.error = true;
+            this.errorMessage = error.message;
+          },
+        });
+      }
+    });
+  }
+
+
+  private showSuccessMessage(): void {
+    const translatedMessage = this.translateService.instant('userDeletedSuccessfully');
+    const translatedClose = this.translateService.instant('close');
+
+    // Open the snackbar with the translated message
+    this.snackBar.open(translatedMessage, translatedClose, {
+      duration: 3000,
+      verticalPosition: 'bottom',
+      horizontalPosition: 'center',
+      panelClass: ['snackbar-success']
+    });
   }
 
 }
