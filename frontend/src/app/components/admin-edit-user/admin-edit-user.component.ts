eb0ce93ed6c3fe611e3a45813b3681b2137617c7
Fisnik Miftari <e11776840@student.tuwien.ac.at>
2024-01-30 02:04:09 +0100

    [10.8][F] admin edit user works with multiple types

--- frontend/src/app/components/admin-edit-user/admin-edit-user.component.ts
+++ frontend/src/app/components/admin-edit-user/admin-edit-user.component.ts
@@ -1,9 +1,9 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {User} from "../../models/User";
-import {UserService} from "../../services/user.service";
-import {QualificationType} from "../../models/Enums";
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { User } from "../../models/User";
+import { UserService } from "../../services/user.service";
+import { QualificationType, UserType } from "../../models/Enums";
 
 @Component({
   selector: 'app-admin-edit-user',
@@ -13,40 +13,91 @@ import {QualificationType} from "../../models/Enums";
 export class AdminEditUserComponent implements OnInit {
   editUserForm: FormGroup;
 
+  isConsumerUser: boolean;
+  isProviderUser: boolean;
+  isAdminUser: boolean;
+
   constructor(
     private fb: FormBuilder,
     private userService: UserService,
     public dialogRef: MatDialogRef<AdminEditUserComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { user: User }
-  ) {}
+  ) {
+    // Set the initial checkbox states based on the injected user data
+    this.isConsumerUser = this.data.user.userType.includes(UserType.ListingConsumer);
+    this.isProviderUser = this.data.user.userType.includes(UserType.ListingProvider);
+    this.isAdminUser = this.data.user.userType.includes(UserType.Administrator);
+  }
 
   ngOnInit(): void {
     this.editUserForm = this.fb.group({
       name: [this.data.user.name, Validators.required],
       email: [this.data.user.email, [Validators.required, Validators.email]],
-      userType: [this.data.user.userType, Validators.required],
       qualification: [this.data.user.qualification],
       receiveEmails: [this.data.user.receiveEmails === null ? 'none' : this.data.user.receiveEmails ? 'yes' : 'no']
-
     });
   }
+
   onSave(): void {
     if (this.editUserForm.valid) {
       let formValue = this.editUserForm.value;
+      let userType: UserType[] = [];
+      if (this.isConsumerUser) userType.push(UserType.ListingConsumer);
+      if (this.isProviderUser) userType.push(UserType.ListingProvider);
+      if (this.isAdminUser) userType.push(UserType.Administrator);
+
       let updatedUser: User = {
         ...this.data.user,
         name: formValue.name,
         email: formValue.email,
-        userType: formValue.userType,
-        qualification: formValue.userType === 'ListingConsumer' ? formValue.qualification : QualificationType.None,
+        userType: userType,
+        qualification: this.isConsumerUser ? formValue.qualification : QualificationType.None,
         receiveEmails: formValue.receiveEmails === 'yes',
         password: ''
       };
-
 
       this.userService.updateUser(updatedUser).subscribe(() => {
         this.dialogRef.close(true);
       });
     }
   }
+
+  toggleUserType(type: UserType) {
+    // Ensure at least one checkbox is always selected
+    if (this.isOnlyOneSelected() && this.isCurrentlySelected(type)) {
+      return; // Do nothing if trying to uncheck the last remaining checkbox
+    }
+
+    switch (type) {
+      case UserType.ListingConsumer:
+        this.isConsumerUser = !this.isConsumerUser;
+        break;
+      case UserType.ListingProvider:
+        this.isProviderUser = !this.isProviderUser;
+        break;
+      case UserType.Administrator:
+        this.isAdminUser = !this.isAdminUser;
+        break;
+    }
+  }
+
+  private isOnlyOneSelected(): boolean {
+    const selectedCount = [this.isConsumerUser, this.isProviderUser, this.isAdminUser].filter(v => v).length;
+    return selectedCount === 1;
+  }
+
+  private isCurrentlySelected(type: UserType): boolean {
+    switch (type) {
+      case UserType.ListingConsumer:
+        return this.isConsumerUser;
+      case UserType.ListingProvider:
+        return this.isProviderUser;
+      case UserType.Administrator:
+        return this.isAdminUser;
+      default:
+        return false;
+    }
+  }
+
+  protected readonly UserType = UserType;
 }
--- frontend/src/app/components/admin-page/admin-page.component.ts
+++ frontend/src/app/components/admin-page/admin-page.component.ts
@@ -57,6 +57,7 @@ export class AdminPageComponent implements OnInit {
             }
           });
 
+          console.log(this.users)
           // Initialize the MatTableDataSource with the fetched users
           this.dataSource = new MatTableDataSource<User>(this.users);
           this.dataSource.paginator = this.paginator;
@@ -98,19 +99,20 @@ export class AdminPageComponent implements OnInit {
 
 
   editUser(userId: number): void {
-    const userToEdit = this.users.find(user => user.id === userId);
-    const dialogRef = this.dialog.open(AdminEditUserComponent, {
-      width: '500px',
-      data: {user: userToEdit}
-    });
+    this.userService.getUserById(userId).subscribe(userToEdit => {
+      const dialogRef = this.dialog.open(AdminEditUserComponent, {
+        width: '500px',
+        data: {user: userToEdit}
+      });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.formatSuccessMessage('userUpdateSuccess')
-        this.refreshTable(); // Create this method to refresh the table data
-      }
-    }, error => {
-      this.formatErrorMessage(error.error.message);
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          this.formatSuccessMessage('userUpdateSuccess');
+          this.refreshTable();
+        }
+      }, error => {
+        this.formatErrorMessage(error.error.message);
+      });
     });
   }
 
